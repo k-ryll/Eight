5141d21e8f15d04e88b98161d38467d872305e64
k-ryll <andreskharyll@gmail.com>
2023-01-14 23:45:43 +0800

    eightt

--- script.js
+++ script.js
@@ -6,47 +6,72 @@ var song = document.getElementById("song");
 var score = 0;
 var jumping = 0;
 
-hole.addEventListener("animationiteration",RanHole)
+hole.addEventListener("animationiteration", RanHole);
 
-function songplay(){
-  song.play()
+function songplay() {
+  song.play();
 }
 songplay();
-function RanHole(){
-  var random = -((Math.random()*350)+150)
-  hole.style.top = random+"px";
+
+function RanHole() {
+  var random = Math.floor(Math.random() * (500 - 150 + 1)) - 500;
+  hole.style.top = random + "px";
   score++;
 }
-var fall = setInterval(function(){
+
+var fall = setInterval(function () {
   var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
-  if(jumping == 0)
-  {
-    bird.style.top = (birdTop+2)+"px";
+
+  console.log(birdTop);
+
+  var speed = 2 + score * 0.1;
+
+  if (jumping == 0) {
+    bird.style.top = birdTop + speed + "px";
   }
-  var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+  var blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+
   var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-  var hTop = (500+holeTop);
-  if((birdTop >450) || ((blockLeft < 50)&&(blockLeft > -50)&&((birdTop<hTop)||(birdTop>hTop +100))))
-  {
+
+  var hTop = 500 + holeTop;
+
+  if (
+    birdTop > 450 ||
+    (blockLeft < 50 &&
+      blockLeft > -50 &&
+      (birdTop < hTop || birdTop > hTop + 120))
+  ) {
     result.style.display = "block";
-    text.innerText = `Your Final score is : ${score}` ;
+    text.innerText = `Your Final score is : ${score}`;
     game.style.display = "none";
-    song.pause()
   }
-},10)
+}, 10);
 
-window.addEventListener("keydown",hop)
-
-function hop()
-{
-  jumpimg = 1;
+function hop() {
+  jumping = 1;
 
   var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
-  if(birdTop> 6)
-  {
-    bird.style.top = (birdTop - 60) + "px";
+
+  if (birdTop > 6) {
+    bird.style.top = birdTop - 60 + "px";
+  } else {
+    bird.style.top = 0;
   }
-  setTimeout(function(){
-    jumping = 0
-  },100)
+
+  setTimeout(function () {
+    jumping = 0;
+  }, 100);
+}
+
+window.addEventListener("keydown", hop);
+
+var isChrome =
+  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome) {
+  $("#iframeAudio").remove();
+} else {
+  $("#playAudio").remove(); // just to make sure that it will not have 2x audio in the background
 }
